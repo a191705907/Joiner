c8d604c7a24887249ddc7fa158a8b38221916fd9
distanceN <863700505@qq.com>
2015-06-13 03:59:11 +0800

    setting up structure ...

---
+++ src/main/resources/createTables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE student(
+  student_id varchar(8) not null,
+  student_name varchar(45) not null,
+  student_password varchar(20) not null,
+  primary key(student_id)
+)engine=InnoDB;
+
+CREATE TABLE organizer(
+  organizer_id int(8) not null auto_increment,
+  organizer_name varchar(45) not null,
+  organizer_password varchar(20) not null,
+  primary key(organizer_id)
+)engine=InnoDB;
+
+CREATE TABLE activity(
+  activity_id int(8) not null auto_increment,
+  activity_name varchar(45) not null,
+  organizer_name varchar(45) not null,
+  primary key(activity_id)
+)engine=InnoDB;
+
+CREATE TABLE student_activity(
+  activity_id int(8) not null,
+  student_id varchar(8) not null,
+  primary key(activity_id, student_id)
+)engine=InnoDB;
